ea8e4605eed85d0eb5f428cf983c724aabb89566
ice-bit <ceticamarco@gmail.com>
2019-08-16 18:33:05 +0200

    Added cli interface

---
+++ README.md
@@ -0,0 +1,19 @@
+# SRead
+
+**S**erial**R**ead is a tool to read and write to serial ports under POSIX systems.
+
+## Installation
+
+Install CMake(>= 3.9) and Boost libraries.
+
+```bash
+mkdir build && cd build
+cmake .. && make
+```
+
+## Usage
+Not ready yet
+
+
+## License
+[GPLv3](https://choosealicense.com/licenses/gpl-3.0/)
--- sread/src/main.cpp
+++ sread/src/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main(void) {
-    auto x = 10;
+using namespace boost::program_options;
 
-    std::cout << "Hello World, x is: " << x << std::endl;
 
-    return 0;
+// Check that opt1 and opt2 aren't specified at the
+// same time
+
+void conflicting_options(const variables_map &vm, const char *opt1, const char *opt2) {
+    if(vm.count(opt1) && !vm[opt1].defaulted() && vm.count(opt2) && !vm[opt2].defaulted())
+        throw std::logic_error(std::string("Conflicting options '") + opt1 + "' and '" + opt2 + "'.");
 }
+
+void option_dependency(const variables_map &vm, const char *for_what, const char *required_option) {
+    if(vm.count(for_what) && !vm[for_what].defaulted())
+        if(vm.count(required_option) == 0 || vm[required_option].defaulted())
+            throw std::logic_error(std::string("Option '") + for_what 
+            + "' requires option '" + required_option + "'.");
+ }
+
+ int main(int argc, char *argv[]) {
+     // Check if arguments were provided at all
+     if(argc < 2) {
+         std::cerr << "ERROR: use: " << argv[0] << " --help to get a list of options!" << std::endl;
+         return -1;
+     }
+
+     try {
+         std::string name;
+
+         options_description desc("Allowed options");
+         desc.add_options()
+         /* Parameters are organized in the following way:
+          * First parameter: describes option name
+          * Second parameter: the actual option
+          * Third one: the description
+          */
+         ("help,h", "Print the helper")
+         ("name,n", value(&name), "Specify your name");
+
+         variables_map vm;
+         store(parse_command_line(argc, argv, desc), vm);
+
+         if(vm.count("help")) {
+             std::cout << desc << std::endl;
+             return 0;
+         }
+         else if(vm.count("name"))
+             std::cout << "Hello " << vm["name"].as<std::string>() << std::endl;
+     }
+     catch(std::exception &e) {
+         std::cerr << e.what() << std::endl;
+     }
+ }
