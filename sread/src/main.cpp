a3d249045e82f02fd2781d914b7ed824be69d6c3
ice-bit <ceticamarco@gmail.com>
2019-08-17 17:29:06 +0200

    fixed indentation

--- sread/src/main.cpp
+++ sread/src/main.cpp
@@ -3,10 +3,8 @@
 
 using namespace boost::program_options;
 
-
 // Check that opt1 and opt2 aren't specified at the
 // same time
-
 void conflicting_options(const variables_map &vm, const char *opt1, const char *opt2) {
     if(vm.count(opt1) && !vm[opt1].defaulted() && vm.count(opt2) && !vm[opt2].defaulted())
         throw std::logic_error(std::string("Conflicting options '") + opt1 + "' and '" + opt2 + "'.");
@@ -15,8 +13,7 @@ void conflicting_options(const variables_map &vm, const char *opt1, const char *
 void option_dependency(const variables_map &vm, const char *for_what, const char *required_option) {
     if(vm.count(for_what) && !vm[for_what].defaulted())
         if(vm.count(required_option) == 0 || vm[required_option].defaulted())
-            throw std::logic_error(std::string("Option '") + for_what 
-            + "' requires option '" + required_option + "'.");
+            throw std::logic_error(std::string("Option '") + for_what  + "' requires option '" + required_option + "'.");
  }
 
  int main(int argc, char *argv[]) {
